cbad877a697f694170438c74e51fbf276f3c9d3c
Aitenov Ilyas <ilyas.aitenov.a@gmail.com>
2021-01-17 21:50:09 +0600

    finished

---
+++ cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"github.com/golangcollege/sessions"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"se09.com/pkg/models/postgresql"
+	"time"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+	session *sessions.Session
+	snippets *postgresql.SnippetModel
+	templateCache map[string]*template.Template
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	connStr := "user=postgres password=root dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+		session: session,
+		snippets: &postgresql.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
